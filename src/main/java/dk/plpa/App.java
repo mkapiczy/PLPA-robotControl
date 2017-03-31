b1bc828151289bc6c66b50acef039e8a76d96ec5
Lasse Have Nielsen <lhn@it-minds.dk>
2017-03-31 10:29:33 +0200

    Fileread from external SCM file.

--- src/main/java/dk/plpa/App.java
+++ src/main/java/dk/plpa/App.java
@@ -4,18 +4,36 @@ package dk.plpa;
 import gnu.mapping.Environment;
 import kawa.standard.Scheme;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import static kawa.standard.Scheme.registerEnvironment;
 
 
 public class App {
+
+
     public static void main(String[] args) {
+
+        FileRead fileRead = new FileRead();
+        String stringToScheme = "";
+
+        try {
+            stringToScheme = fileRead.readFile("src/main/scheme/dk.plpa/test.scm", StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         registerEnvironment();
         Environment.setCurrent(new Scheme().getEnvironment());
         Object x;
 
-        x = Scheme.eval("(+ 3 2)", Environment.getCurrent());
+        x = Scheme.eval(stringToScheme, Environment.getCurrent());
 
         System.out.println(x);
     }
+
+
+
 
 }
