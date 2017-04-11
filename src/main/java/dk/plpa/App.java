e909b5f74aad0ca28f7adad9c663a02e9bacade3
mkapiczy <mix.kap@gmail.com>
2017-04-11 15:48:37 +0200

    Basic animation example with use of JavaFX (for future modifications)

--- src/main/java/dk/plpa/App.java
+++ src/main/java/dk/plpa/App.java
@@ -4,9 +4,17 @@ package dk.plpa;
 import dk.plpa.scheme.SchemeConfigurer;
 import dk.plpa.scheme.SchemeProcedure;
 import gnu.math.IntNum;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 
-public class App {
+public class App extends Application {
 
 
     public static void main(String[] args) {
@@ -14,15 +22,53 @@ public class App {
         SchemeConfigurer schemeConfigurer = new SchemeConfigurer("src/main/scheme/dk.plpa/factorial.scm");
         schemeConfigurer.configureSchemeEnvironment();
 
-        int currentState = 0;
-        while(currentState<10){
-            SchemeProcedure incrementProcedure = new SchemeProcedure("increment");
-            currentState = ((IntNum) incrementProcedure.apply1(currentState)).intValue();
-            System.out.println(currentState);
-        }
+        launch(args);
     }
 
 
+    @Override
+    public void start(Stage theStage) throws Exception {
+        theStage.setTitle("Timeline Example");
+
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        theStage.setScene(theScene);
+
+        Canvas canvas = new Canvas(512, 512);
+        root.getChildren().add(canvas);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
 
+        new AnimationTimer() {
+            private int currentState = 0;
+            private long lastUpdate = 0;
+
+            public void handle(long currentNanoTime) {
+                if (currentNanoTime - lastUpdate >= 500000000){
+                    SchemeProcedure incrementProcedure = new SchemeProcedure("increment");
+                    currentState = ((IntNum) incrementProcedure.apply1(currentState)).intValue();
+                    System.out.println(currentState);
+                    drawShapes(gc, currentState);
+
+                    if (currentState == 10) {
+                        this.stop();
+                    }
+                    lastUpdate = currentNanoTime;
+                }
+
+            }
+        }.start();
+
+        theStage.show();
+
+    }
+
+    private void drawShapes(GraphicsContext gc, Integer i) {
+        gc.clearRect(0, 0, 512, 512);
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.fillText(i.toString(), 250, 250);
+    }
 }
