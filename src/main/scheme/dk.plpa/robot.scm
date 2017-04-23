89bea22101bfe3630cb8be327ddb1b623dda7f91
mkapiczy <mix.kap@gmail.com>
2017-04-23 13:06:02 +0200

    Basic robot object functionality implemented in scheme.

---
+++ src/main/scheme/dk.plpa/robot.scm
@@ -0,0 +1,93 @@
+(define (robot x y direction errorCode)
+  (letrec (
+           (getX    (lambda () x))
+           (getY    (lambda () y))
+           (getDirection (lambda () direction))
+           (getErrorCode (lambda () errorCode))
+           (moveForward (lambda ()
+                          (if (isMovementAllowed)
+                              (robot
+                               (getNextXPosition "FORWARD")
+                               (getNextYPosition "FORWARD")
+                               direction
+                               errorCode)
+                              (robot
+                               x
+                               y
+                               direction
+                               -1))
+                          ))
+           (turnRight (lambda ()
+                        (robot
+                         x
+                         y
+                         (getNextDirection "RIGHT")
+                         errorCode)))
+           (turnLeft (lambda ()
+                       (robot
+                        x
+                        y
+                        (getNextDirection "LEFT")
+                        errorCode)))
+
+           (type-of (lambda () 'robot))
+
+
+           (getNextXPosition (lambda(movementDirection)
+                               (if (eq? "FORWARD" movementDirection)
+                                   (cond
+                                     ((eq? "N" direction) x)
+                                     ((eq? "E" direction) (+ x 1))
+                                     ((eq? "S" direction) x)
+                                     ((eq? "W" direction) (- x 1))
+                                     )
+                                   (x)
+                                   )))
+           (getNextYPosition (lambda(movementDirection)
+                               (if (eq? "FORWARD" movementDirection)
+                                   (cond
+                                     ((eq? "N" direction) (- y 1))
+                                     ((eq? "E" direction) y)
+                                     ((eq? "S" direction) (+ y 1))
+                                     ((eq? "W" direction) y)
+                                     )
+                                   (x)
+                                   )))
+
+           (getNextDirection (lambda (rotationDirection)
+                               (if (eq? rotationDirection "RIGHT")
+                                   (cond
+                                     ((eq? "N" direction) "E")
+                                     ((eq? "E" direction) "S")
+                                     ((eq? "S" direction) "W")
+                                     ((eq? "W" direction) "N"))
+                                   (cond
+                                     ((eq? "N" direction) "W")
+                                     ((eq? "W" direction) "S")
+                                     ((eq? "S" direction) "E")
+                                     ((eq? "E" direction) "N"))
+                                   )
+                               ))
+
+           (isMovementAllowed (lambda ()
+                                #t
+                                )))
+    (lambda (message)
+      (cond ((eq? message 'getX) getX)
+            ((eq? message 'getY) getY)
+            ((eq? message 'getDirection)  getDirection)
+            ((eq? message 'moveForward)  moveForward)
+            ((eq? message 'turnRight)  turnRight)
+            ((eq? message 'turnLeft)  turnLeft)
+            ((eq? message 'type-of) type-of)
+            (else (error "Message not understood"))))
+    )
+  )
+
+
+
+
+(define (send message object . args)
+  (let ((method (object message)))
+    (cond ((procedure? method) (apply method args))
+          (else (error "Error in method lookup " method)))))
