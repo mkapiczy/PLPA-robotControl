0cc603153ec7bfd014381ea5d18a0fe343e60881
mkapiczy <mix.kap@gmail.com>
2017-05-14 22:27:57 +0200

    [Java] - Animation View changing width of canvas to suit the floor width.

--- src/main/java/dk/plpa/gui/views/ViewsDimensions.java
+++ src/main/java/dk/plpa/gui/views/ViewsDimensions.java
@@ -3,7 +3,7 @@ package dk.plpa.gui.views;
 
 public class ViewsDimensions {
 
-    public final static int ANIMATION_VIEW_WIDTH=512;
+    public final static int ANIMATION_VIEW_WIDTH=860;
     public final static int ROBOT_PROGRAMMING_VIEW_WIDTH=400;
     public final static int COMMANDS_LIST_VIEW_WIDTH=320;
     public final static int VIEWS_HEIGHT=640;
