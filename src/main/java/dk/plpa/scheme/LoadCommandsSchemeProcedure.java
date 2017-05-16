81b61786278bccf20a904219fb0340119d78600f
mkapiczy <mix.kap@gmail.com>
2017-05-16 21:56:42 +0200

    [Java] - refactoring removal of some old stuff

--- src/main/java/dk/plpa/scheme/LoadCommandsSchemeProcedure.java
+++ src/main/java/dk/plpa/scheme/LoadCommandsSchemeProcedure.java
@@ -11,10 +11,7 @@ public class LoadCommandsSchemeProcedure {
     private static SchemeProcedure loadProcedure = new SchemeProcedure("loadCommands");
 
     public static void loadProcedures(RobotState startingPosition, List<CommandListItem> commands) {
-        // TODO Something is wrong with loading the procedures. Mapped procedure names strings are not recognized by Scheme.
         String s = SchemeTypesMapper.createLoadCommandSchemeArgument(startingPosition, commands);
-//        String s = "(list (list 0 8 \"E\") (list MoveForward 8) (list TurnLeft 1) (list MoveForward 7) (list TurnLeft 1) (list MoveForward 2) (list TurnLeft 1) (list PickObject \"P1\") (list TurnLeft 1) (list MoveForward 2) (list TurnRight 1) (list DropObject 1) )";
-
         loadProcedure.apply1(s);
     }
 }
