7cb3c94ee56f156a36ea7f31172452fd94eacc89
mkapiczy <mix.kap@gmail.com>
2017-05-19 07:39:52 +0200

    [Scheme] FloorUtilTest - testing out of bouns scenarios

--- src/main/scheme/dk.plpa/tests/FloorUtilTest.scm
+++ src/main/scheme/dk.plpa/tests/FloorUtilTest.scm
@@ -3,6 +3,7 @@
  (rough-draft console-test-runner))
 
 (include "../FloorUtil.scm")
+;Test suite requires (include "../tests/FloorPlanTest1.scm") in FloorUtil
 
 
 (define-test-suite foo
@@ -10,12 +11,18 @@
     (assert-eqv? (getFactoryFloor) factoryFloor))
   (define-test getTile1-test
     (assert-eqv? (getTile 0 0) 'o))
-  (define-test getTile1-test
+  (define-test getTile2-test
     (assert-eqv? (getTile 1 0) 'A))
-  (define-test getTile1-test
+  (define-test getTile3-test
     (assert-eqv? (getTile 1 1) 'i))
-  (define-test getTile1-test
+  (define-test getTile4-test
     (assert-eqv? (getTile 3 2) 'P))
+  (define-test getTileOutOfBound1-test
+    (assert-eqv? (getTile -1 -1) '()))
+  (define-test getTileOutOfBound2-test
+    (assert-eqv? (getTile 60 0) '()))
+  (define-test getTileOutOfBound3-test
+    (assert-eqv? (getTile 0 60) '()))
   )
  
 
