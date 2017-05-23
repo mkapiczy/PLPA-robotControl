229a769e1f4e289ce0e6e08cd034e87316b7f397
mkapiczy <mix.kap@gmail.com>
2017-05-23 20:37:27 +0200

    [Java] SchemeTypesMapper - removal of unused methods

--- src/main/java/dk/plpa/scheme/SchemeTypesMapper.java
+++ src/main/java/dk/plpa/scheme/SchemeTypesMapper.java
@@ -11,40 +11,6 @@ import java.util.Objects;
 public class SchemeTypesMapper {
 
 
-    public static <T> String javaListToSchemeList(List<T> javaList) {
-        if (javaList == null || javaList.isEmpty()) {
-            return "'()";
-        } else {
-            String s = "'(" + javaList.toString().replace(",", "").replace("[", "").replace("]", "") + ')';
-            return s;
-        }
-    }
-
-    public static <T> String javaTwoElementListToSchemeCons(List<T> javaList) {
-        if (javaList == null || javaList.isEmpty()) {
-            return "'()";
-        } else if(javaList.size()==2){
-            String s = "(cons " + javaList.toString().replace(",", "").replace("[", "").replace("]", "").replace("null","'()") + ')';
-            return s;
-        } else{
-            return null;
-        }
-    }
-
-
-    public static <T> String javaListOfListToSchemeListOfPairs(List<List<T>> javaList) {
-        if (javaList == null || javaList.isEmpty()) {
-            return "'()";
-        } else {
-            String s = "'(";
-            for (int i = 0; i < javaList.size(); i++) {
-                    s += javaTwoElementListToSchemeCons(javaList.get(i));
-                    s+= " ";
-            }
-            s += ')';
-            return s;
-        }
-    }
 
     public static <T> String createLoadCommandSchemeArgument(RobotState startingPosition, List<CommandListItem> javaList) {
         if (javaList == null || javaList.isEmpty()) {
